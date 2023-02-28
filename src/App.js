d447e79372f417b29cb273fc72ecfd2b3c810053
venkatsp <venkatsp2002@gmail.com>
2023-02-28 11:20:23 +0530

    app js changed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
 import React from 'react';
-import {Route, Routes} from 'react-router-dom'
-import Project from './components/Project';
-import About from './components/About';
-import Skills from './components/Skills';
+// import {Route, Routes} from 'react-router-dom'
+// import Project from './components/Project';
+// import About from './components/About';
+// import Skills from './components/Skills';
 import Main from './Main';
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/project" element={<Project/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/skills" element={<Skills/>} />
-    </Routes>
+   <div>
+    <Main/>
+   </div>
   );
 }
 
